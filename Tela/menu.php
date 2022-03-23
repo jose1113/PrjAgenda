7096be8ebf8618d11124f278d15a3d893ab8d2e6
jose1113 <joseigor113@gmail.com>
2022-03-23 18:13:57 -0300

    Arquivo formatado

--- Tela/menu.php
+++ Tela/menu.php
@@ -1,30 +1,20 @@
-	<div class="masthead">
-			<h3>
-				CADASTRO DE USUÁRIO
-			</h3>
-		</div>
-		
-		<nav>
-			<ul class="nav nav-justified">
-				<li>
-					<a href="#">
-							INÍCIO
-						</a>
-				</li>
-				<li>
-					<a href="#">
-						CADASTRAR
-					</a>
-				 </li>
-				<li>
-					<a href="#">
-						LOGIN
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						CONSULTAR
-					</a>
-				</li>
-			</ul>
-		</nav>
+<div class="masthead">
+	<h3>CADASTRO DE USUÁRIO</h3>
+</div>
+	
+<nav>
+	<ul class="nav nav-justified">
+		<li>
+			<a href="#">INÍCIO</a>
+		</li>
+		<li>
+			<a href="cadastrar.php">CADASTRAR</a>
+		</li>
+		<li>
+			<a href="#">LOGIN</a>
+		</li>
+		<li>
+			<a href="#">CONSULTAR</a>
+		</li>
+	</ul>
+</nav>
