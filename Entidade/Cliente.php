89b1290af7cc1f7612f3a3a44e37604ca6c283ff
jose1113 <joseigor113@gmail.com>
2022-03-23 18:12:28 -0300

    Comments with construct

--- Entidade/Cliente.php
+++ Entidade/Cliente.php
@@ -3,7 +3,28 @@ class cliente{
     private $codigo;
     private $nome;
     private $cidade;
-    
-}
 
-?>
+    
+//     function __construct() {
+//         $this->codigo = isset($_POST['codigo']) ? $_POST['codigo'] : null;
+//         $this->nome = isset($_POST['nome']) ? $_POST['nome'] : null;
+//         $this->cidade = isset($_POST['cidade']) ? $_POST['cidade'] : null;
+//     }
+
+//     function start() {
+//         if (empty($this->nome) || empty($this->cidade)) {
+//             throw new Exception("Preencha os campos vazios");
+//         }
+
+//         else
+//         {
+//             // Do some stuiff
+//             echo "Cadastro realizado";
+//         }
+//     }
+// }
+
+// $register = new cliente();
+// if(!empty($_POST)){
+//     $register->start();
+}
